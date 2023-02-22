d3e26fb217bf01663ef67db72fd8d4855794b08c
eavalo14 <avaloserick97@gmail.com>
2023-02-22 13:00:44 -0800

    working routes for category api

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -31,6 +31,12 @@ const productData = [
     stock: 22,
     category_id: 2,
   },
+  {
+    product_name: 'Running Shorts',
+    price: 29.99,
+    stock: 22,
+    category_id: 2,
+  },
 ];
 
 const seedProducts = () => Product.bulkCreate(productData);
